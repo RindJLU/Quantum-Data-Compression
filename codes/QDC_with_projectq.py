6d9a7da5f4612c6c762542573ffb901c891d2f6c
Yufeng <wuyf1114@mails.jlu.edu.cn>
2018-01-25 20:44:55 +0800

    Edited the codes.

--- codes/QDC_with_projectq.py
+++ codes/QDC_with_projectq.py
@@ -1,80 +1,90 @@
-# coding: utf-8
-
 import numpy as np
 import projectq
-from projectq.ops import Measure, H, X, C
-from projectq.meta import Control
-from projectq.backends import Simulator
+from projectq.ops import Measure, H, X, C, All
 
-class qd_autocompresser():
+
+class QImEncoder(object):
+    """Store classical data/image in quantum state.
+
+    Using Projectq, a quantum computational simulating module , to demonstrate
+    information representation in qubits. First encoding classical information
+    into quantum state, then retrieving information through measurement.
+
+    Args:
+        data (ndarray): a array of one bit binary number where N is the length of the data.
+        dimension (int): the number of qubits required to represent the data.
+
+        eng:
+        qubits:
+
+    Returns:
+
+
+    """
+
     def __init__(self, data):
+        """Initialing QImEncoder with some args."""
+
         self.data = data
-        # input classical data, where N is the length of the data.
-        self.dimension = np.log2(len(self.data))
-        self.dict_V_P = {}
+        self.posRef = []  # position reference
+        self.dimension = int(np.ceil(np.log2((len(self.data)))))
+        for num in range(len(data)):
+            ref_temp = bin(num)[2:]  # temporary reference
+            # fun bin() would return a str head with '0b', which should be removed.
+            while self.dimension - len(ref_temp) != 0:  # uniform the ref repression.
+                ref_temp = '0' * (self.dimension - len(ref_temp)) + ref_temp  # if dim=3, then convert '0' to '000'.
+            self.posRef.append(ref_temp)
 
-        # projectq
+        """using projectq module to create qubits"""
         self.eng = projectq.MainEngine()
-        self.qubits = ()
-        # add a auxiliary qubit:
-        self.qubits += (self.eng.allocate_qubit()[0],)
-        for i in range(int(self.dimension)):
-            self.qubits += (self.eng.allocate_qubit()[0],)
+        self.qubits = self.eng.allocate_qureg(self.dimension + 1)  # create one auxiliary qubit.
 
-        # the follows operation would produce a dictionary between position and the corresponding
-        # massage that the position stores
-        # For example:{'00': '1', '01': '0', '10': '0', '11': '0'}
-        index = []
-        for i in range(len(self.data)):
-            if i == 0:
-                index.append(1)
-            else:
-                for j in range(int(self.dimension)+1):
-                    while i >= pow(2, j-1) and i <= pow(2, j):
-                        index.append(j+1)
-                        break
-        for i in range(len(self.data)):
-            a = bin(i)[2:2+index[i]]
-            while len(a) < max(index) - 1:
-                a = '0' + a
-            self.dict_V_P[a] = self.data[i]
+    def quStatePrep(self, cheat=False):
+        """prepare quantum state for information storing"""
 
-    def Control(self):
-        for i in range(len(self.qubits)):
-            if i != 0:
-                H | self.qubits[i]
+        All(H) | self.qubits[1:]# prepare the qubits for position reference, the first(qubit[0]) remains
+        # unchanged for representing corresponding value.
 
+        for p_ref_index in range(len(self.data)):  # p_ref_index is the index of the position of the data.
+            while self.data[p_ref_index] == 1: # only the data in a position is 1, an X operation will be implemented.
+                for i in self.dimension:  # reshape the qubit to represent the corresponding position.
+                    if self.posRef[p_ref_index][i] == 1:
+                        X | qubits[i]
 
-        for i in self.dict_V_P:
-            if self.dict_V_P[i] == '1':
-                for j in range(len(i)):
-                    if i[j] == '0':
-                        X | self.qubits[j + 1]
-                # with Control(self.eng, self.qubits[1]):
-                #     with Control(self.eng, self.qubits[2]):
-                #         with Control(self.eng, self.qubits[3]):
-                #             X | self.qubits[0]
+                C(X, self.dimension) | (self.qubits[1:], self.qubits[0])  # controlled X operation to flip |0> to |1>.
 
-                C(X, self.dimension) | (self.qubits[1:], self.qubits[0])
+                for i in self.dimension:  # reshape the qubit to its original state
+                    if self.posRef[p_ref_index][i] == 1:
+                        X | qubits[i]
 
-                for j in range(len(i)):
-                    if i[j] == '0':
-                        X | self.qubits[j + 1]
+        if cheat:
+            self._cheat()
+        else:
+            self._measure()
 
+    def _cheat(self):
 
-    def Measure(self):
-        for i in range(len(self.qubits)):
-            Measure | self.qubits[i]
+        self.eng.flush()
+        order, vec = self.eng.backend.cheat()
+
+        Measure | self.qubits
+
+        result = ''
+        for qubit in self.qubits:
+            result = result + str(int(qubit))
+        print('Measured {} in the position {}'.format(result[0], result[1:]))
+
+    def _measure(self):
+        Measure | self.qubits
 
         self.eng.flush()
 
         result = ''
         for qubit in self.qubits:
             result = result + str(int(qubit))
-        print('Measured ' + result[0] + ' in the position ' + result[1:])
+        print('Measured {} in the position {}'.format(result[0], result[1:]))
 
-
-a = qd_autocompresser('10101010')
-print(type(a.qubits[0]))
-a.Control()
-a.Measure()
+if __name__ == '__main__':
+    a = QImEncoder('1010')
+    print(type(a.qubits[0]))
+    a.quStatePrep()
