5f9cb32b1aabe9753a6132ec4553f54b50615a94
Yufeng <wuyf1114@mails.jlu.edu.cn>
2018-01-26 19:50:30 +0800

    Edited the codes.

--- codes/QDC_with_projectq.py
+++ codes/QDC_with_projectq.py
@@ -26,36 +26,33 @@ class QImEncoder(object):
         """Initialing QImEncoder with some args."""
 
         self.data = data
-        self.posRef = []  # position reference
-        self.dimension = int(np.ceil(np.log2((len(self.data)))))
-        for num in range(len(data)):
-            ref_temp = bin(num)[2:]  # temporary reference
-            # fun bin() would return a str head with '0b', which should be removed.
-            while self.dimension - len(ref_temp) != 0:  # uniform the ref repression.
-                ref_temp = '0' * (self.dimension - len(ref_temp)) + ref_temp  # if dim=3, then convert '0' to '000'.
-            self.posRef.append(ref_temp)
+        self.dim = self.data.shape
+        self.numQuRep = int(np.log2(self.dim[0] * self.dim[1]))
+        self.numQuCol = 1
+        self.posRef = self._posRef()  # position reference
 
         """using projectq module to create qubits"""
         self.eng = projectq.MainEngine()
-        self.qubits = self.eng.allocate_qureg(self.dimension + 1)  # create one auxiliary qubit.
+        self.qubits = self.eng.allocate_qureg(self.numQuCol + self.numQuRep)  # create one auxiliary qubit.
 
     def quStatePrep(self, cheat=False):
         """prepare quantum state for information storing"""
 
-        All(H) | self.qubits[1:]  # prepare the qubits for position reference, the first(qubit[0]) remains
+        All(H) | self.qubits[self.numQuCol:]  # prepare the qubits for position reference, the first(qubit[0]) remains
         # unchanged for representing corresponding value.
 
-        for p_ref_index in range(len(self.data)):  # p_ref_index is the index of the position of the data.
-            if self.data[p_ref_index] == 1:  # only the data in a position is 1, an X operation will be implemented
-                for i in range(self.dimension):  # reshape the qubit state to represent the corresponding position.
-                    if self.posRef[p_ref_index][i] == '0':
-                        X | self.qubits[i + 1]
+        for pRefRow in range(self.dim[0]):  # p_ref_index is the index of the position of the data.
+            for pRefCol in range(self.dim[1]):
+                if self.data[pRefRow][pRefCol] == 1:  # only the data in a position is 1, an X operation will be implemented
+                    for i in range(self.numQuRep):  # reshape the qubit state to represent the corresponding position.
+                        if self.posRef[pRefRow][pRefCol][i] == 0:
+                            X | self.qubits[i + 1]
 
-                C(X, self.dimension) | (self.qubits[1:], self.qubits[0])  # controlled X operation to flip |0> to |1>.
+                    C(X, self.numQuRep) | (self.qubits[1:], self.qubits[0])  # controlled X operation to flip |0> to |1>.
 
-                for i in range(self.dimension):  # reshape the qubit to its original state
-                    if self.posRef[p_ref_index][i] == '0':
-                        X | self.qubits[i + 1]
+                    for i in range(self.numQuRep):  # reshape the qubit to its original state
+                        if self.posRef[pRefRow][pRefCol][i] == 0:
+                            X | self.qubits[i + 1]
 
         if cheat:
             self._cheat()
@@ -72,7 +69,7 @@ class QImEncoder(object):
         result = ''
         for qubit in self.qubits:
             result = result + str(int(qubit))
-        print('Measured {} in the position {}'.format(result[0], result[1:]))
+        print('Measured {} in the position {}, {}'.format(result[0], result[1:int(np.log2(self.dim[0]))+1], result[int(np.log2(self.dim[0]))+1:]))
 
     def _measure(self):
         Measure | self.qubits
@@ -81,10 +78,46 @@ class QImEncoder(object):
         result = ''
         for qubit in self.qubits:
             result = result + str(int(qubit))
-        print('Measured {} in the position {}'.format(result[0], result[1:]))
-        # print(self.posRef, self.dimension)
-        # print(result)
+        print('Measured {} in the position {}, {}'.format(result[0], result[1:int(np.log2(self.dim[0]))+1], result[int(np.log2(self.dim[0]))+1:]))
+
+
+    def _posRef(self):
+        posTemp = [[], []]
+        for pos in range(len(self.dim)):
+            for num in range(self.dim[pos]):
+                ref_temp = bin(num)[2:]  # temporary reference
+                # fun bin() would return a str head with '0b', which should be removed.
+                while int(np.log2(self.dim[pos])) - len(ref_temp) != 0:  # uniform the ref repression.
+                    ref_temp = '0' * (int(np.log2(self.dim[pos])) - len(
+                        ref_temp)) + ref_temp  # if dim=3, then convert '0' to '000'.
+                posTemp[pos].append(ref_temp)
+                # posTemp has two part, one stores the binary position of row, the other stores colome.
+        # print(posTemp)
+        posList = []
+        for row in range(len(posTemp[0])):
+            for col in range(len(posTemp[1])):
+                temp_list = posTemp[0][row] + posTemp[1][col]
+                for i in temp_list:
+                    posList.append(int(i))
+        return np.array(posList).reshape([self.dim[0], self.dim[1], self.numQuRep])  # position reference
 
 if __name__ == '__main__':
-    a = QImEncoder(np.array([1, 0, 1, 0]))
+    a = QImEncoder(np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             ]))
     a.quStatePrep()
+
